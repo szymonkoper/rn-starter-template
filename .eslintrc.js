768fbed3479f8e79ec62bf18c4f7e7ba78f22f72
Szymon Koper <sz.koper@gmail.com>
2019-11-07 17:14:40 +0100

    Configures eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,67 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es6: true,
+    'jest/globals': true,
+  },
+  extends: ['airbnb'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'better-styled-components',
+    'import',
+    'jest',
+    'jsx-a11y',
+    'react-hooks',
+    'react-native',
+    'react',
+  ],
+  rules: {
+    'better-styled-components/sort-declarations-alphabetically': 'warn',
+    'comma-dangle': ['error', 'never'],
+    'global-require': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+      },
+    ],
+    'import/no-extraneous-dependencies': ['error', {packageDir: './'}],
+    'no-return-assign': ['error', 'except-parens'],
+    'no-unused-vars': [
+      'error',
+      {argsIgnorePattern: '^_', ignoreRestSiblings: true},
+    ],
+    'react-native/no-inline-styles': 'error',
+    'react-native/no-unused-styles': 'error',
+    'react/forbid-foreign-prop-types': 'off',
+    'react/jsx-filename-extension': ['error', {extensions: ['.js']}],
+    'react/jsx-props-no-spreading': 'warn',
+    semi: ['error', 'never'],
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+      },
+    ],
+  },
 };
