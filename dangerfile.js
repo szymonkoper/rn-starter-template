9a4d6fd370dd3d49b143a12761da060932f2e69d
Szymon Koper <sz.koper@gmail.com>
2019-11-30 14:35:53 +0100

    Improves danger messages: package version and dependencies updates.

--- dangerfile.js
+++ dangerfile.js
@@ -1,12 +1,43 @@
-import { danger, message, warn } from 'danger'
+import { danger, fail, message, schedule, warn } from 'danger'
 
-const modifiedMD = danger.git.modified_files.join('- ')
-message(`Changed Files in this PR: \n - ${modifiedMD}`)
+function verifyUpdatedLockedPackages() {
+  const modifiedFiles = danger.git.modified_files
+  const packageChanged = modifiedFiles.includes('package.json')
+  const lockfileChanged = modifiedFiles.includes('yarn.lock')
 
-const packageChanged = danger.git.modified_files.includes('package.json')
-const lockfileChanged = danger.git.modified_files.includes('yarn.lock')
-if (packageChanged && !lockfileChanged) {
-  warn(
-    'Changes were made to package.json, but not to yarn.lock - <i>Perhaps you need to run `yarn install`?</i>'
-  )
+  if (!packageChanged && !lockfileChanged) {
+    message('No dependencies versions were changed')
+  } else if (!packageChanged && lockfileChanged) {
+    message('Dependencies locked versions were updated')
+  } else if (packageChanged && !lockfileChanged) {
+    warn(
+      'Changes were made to `package.json`, but not to `yarn.lock`. Forgot to install them?'
+    )
+  } else {
+    message('Dependencies were updated')
+  }
 }
+
+async function verifyPackageVersionUpdate() {
+  try {
+    const { diff } = await danger.git.diffForFile('package.json')
+
+    const PACKAGE_VERSION_OLD_PATTERN = /^-\s+"version":\s"(\d+[.\d+]*)".*$/m
+    const PACKAGE_VERSION_NEW_PATTERN = /^\+\s+"version":\s"(\d+[.\d+]*)".*$/m
+
+    const oldMatch = PACKAGE_VERSION_OLD_PATTERN.exec(diff)
+    const newMatch = PACKAGE_VERSION_NEW_PATTERN.exec(diff)
+    if (oldMatch && newMatch) {
+      message(`Package version updated from ${oldMatch[1]} to ${newMatch[1]}`)
+    } else {
+      warn('Package version was not updated. Is it intentional?')
+    }
+  } catch (error) {
+    fail(
+      `Could not verify package version. Error message: <i>${error.message}</i>`
+    )
+  }
+}
+
+verifyUpdatedLockedPackages()
+schedule(verifyPackageVersionUpdate)
