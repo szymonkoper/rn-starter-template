9432300aed4e25dde4fd9a037f75249e6c3a0d76
Szymon Koper <sz.koper@gmail.com>
2019-12-01 01:12:46 +0100

    Uses absolute paths in few more imports

--- src/hocs/withApolloGraphQlProvider.js
+++ src/hocs/withApolloGraphQlProvider.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ApolloProvider } from '@apollo/react-hooks'
-import client from '../api/graphql'
+import client from 'api/graphql'
 
 export default Component => props => (
   <ApolloProvider client={client}>
--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,15 @@ import { applyMiddleware, createStore } from 'redux'
 import reduxThunk from 'redux-thunk'
 import { createLogger } from 'redux-logger'
 import { persistStore } from 'redux-persist'
-import rootReducer from '../reducers'
+import rootReducer from 'reducers'
 
 const store = (() => {
   let storeWithMiddleware = null
 
-  storeWithMiddleware = applyMiddleware(reduxThunk, createLogger())(
-    createStore
-  )(rootReducer)
+  storeWithMiddleware = applyMiddleware(
+    reduxThunk,
+    createLogger()
+  )(createStore)(rootReducer)
 
   return storeWithMiddleware
 })()
