65ed4295a186cf2699dcde95d6925aa20c936404
Szymon Koper <sz.koper@gmail.com>
2019-12-05 21:43:10 +0100

    Updates DevScreen test and snapshot

--- src/screens/DevScreen/DevScreen.test.js
+++ src/screens/DevScreen/DevScreen.test.js
@@ -1,16 +1,33 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react'
-import { fireEvent, render } from 'react-native-testing-library'
+import {
+  act,
+  fireEvent,
+  flushMicrotasksQueue,
+  render
+} from 'react-native-testing-library'
 import { I18nextProvider } from 'react-i18next'
 import { MockedProvider } from '@apollo/react-testing'
 import i18next from 'i18n/__test__/testInitialize'
 import DevScreen from './DevScreen'
 
+jest.mock('notifications', () => ({
+  createChannel: jest.fn(),
+  openSettings: jest.fn(),
+  showPermissionAlert: jest.fn(),
+  hasPermission: jest.fn(() => Promise.resolve(true)),
+  requestPermission: jest.fn(() => Promise.resolve(true)),
+  showNotification: jest.fn(),
+  Importance: { Urgent: 'Urgent' }
+}))
+
+jest.useFakeTimers()
+
 describe('DevScreen', () => {
   let props = null
   let renderedScreen = null
 
-  beforeEach(() => {
+  beforeEach(async () => {
     props = {
       language: 'pl',
       updateLanguage: jest.fn()
@@ -24,6 +41,11 @@ describe('DevScreen', () => {
         </MockedProvider>
       </I18nextProvider>
     )
+
+    act(() => {
+      jest.runAllTimers()
+    })
+    await flushMicrotasksQueue()
   })
 
   it('renders correctly', () => {
